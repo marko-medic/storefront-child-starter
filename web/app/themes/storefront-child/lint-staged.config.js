1bd232a8a87ec9b9c280139286d17675fee2bc2a
Marko <m.medic@levi9.com>
2021-04-11 23:55:13 +0200

    Update lint-staged.config.js

--- web/app/themes/storefront-child/lint-staged.config.js
+++ web/app/themes/storefront-child/lint-staged.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   linters: {
-    'sfc-assets/**/*.+(js|ts|md|ts|css|sass|less|graphql|yml|yaml|scss|json|vue)': [
+    'sfc-assets/**/*.+(js|ts|md|css|sass|less|graphql|yml|yaml|scss|json|vue)': [
       'eslint --fix',
       'prettier --write',
       'git add',
